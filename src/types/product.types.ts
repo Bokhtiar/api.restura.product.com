615b8d55772f56bdd73250b6e7ca1c6b17d8cda5
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-02-02 10:25:31 +0600

    restura product service product type and validation done

---
+++ src/types/product.types.ts
@@ -0,0 +1,26 @@
+import { Types } from "mongoose";
+
+export interface IProduct {
+  _id: Types.ObjectId;
+  name: string;
+  price: string;
+  components: string;
+  description: string;
+  image: string;
+  cooking_time: string;
+  offer_start?: string;
+  offer_end?: string;
+  is_published: string;
+}
+
+export interface IProductCreateUpdate {
+  name: string;
+  price: string;
+  components: string;
+  description: string;
+  image: string;
+  cooking_time: string;
+  offer_start?: string;
+  offer_end?: string;
+  is_published: string;
+}
---
+++ src/validators/product.validators.ts
@@ -0,0 +1,44 @@
+import Schema from "async-validator";
+import { NextFunction, Request, Response } from "express";
+
+/* Resource createUpdate validaor */
+const createUpdate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const descriptor = <any>{
+    name: {
+      type: "string",
+      required: true,
+      message: "Name is required.",
+    },
+    price: {
+      type: "string",
+      required: true,
+      message: "Price is required.",
+    },
+    components: {
+      type: "string",
+      required: true,
+      message: "Components is required.",
+    },
+  };
+
+  /* Execute the validator */
+  const validator = new Schema(descriptor);
+
+  validator.validate({ ...req.body }, (errors: any) => {
+    if (errors) {
+      return res.status(422).json({
+        status: false,
+        errors,
+      });
+    }
+    next();
+  });
+};
+
+export const productValidators = {
+  createUpdate,
+};
